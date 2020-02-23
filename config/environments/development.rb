03b1af68a4a855b84c73c188283e9f94008706fe
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-23 12:28:18 +0100

    adjusted action mailer production settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,12 +63,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => 'utf-8'
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = { from: 'nyagaandreroy@gmail.com' }
 
-  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.smtp_settings = {
-   address: 'smtp.gmail.com',
+   address: 'smtp.gmail.com', 
    port: 587,
    authentication: 'plain',
    enable_starttls_auto: true,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -98,25 +98,26 @@ Rails.application.configure do
   }
 
   # ActionMailer::Base.delivery_method = :smtp
+  
   # ActionMailer::Base.smtp_settings = {
   #  address: 'smtp.sendgrid.net',
   #  port: '25',
-  #  domain: 'heroku.com',
+  #  domain: 'own-your-life-campaign.herokuapp.com',
   #  user_name: ENV['SENDGRID_USERNAME'],
   #  password: ENV['SENDGRID_PASSWORD'],
   #  authentication: 'plain',
   #  enable_starttls_auto: true
   # }
-
-   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-   address: 'smtp.gmail.com',
-   port: '587',
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+   address: 'smtp.sendgrid.net',
+   port: 587,
+   domain: 'own-your-life-campaign.herokuapp.com',
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
    authentication: 'plain',
-   enable_starttls_auto: true,
-   user_name: ENV['GMAIL_USERNAME'],
-   password: ENV['GMAIL_PASSWORD'],
-   openssl_verify_mode: 'none'
+   enable_starttls_auto: true
   }
 
 
