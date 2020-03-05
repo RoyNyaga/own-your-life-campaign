a701fb3cab80f07c8deb796eb5bc543b1336d9a5
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-05 19:40:34 +0100

    changed prodection smpt port configuration to 25

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
    :password => ENV['SENDGRID_PASSWORD'],
    :domain => 'own-your-life-campaign.herokuapp.com',
    :address => 'smtp.sendgrid.net',
-   :port => 465,
+   :port => 25,
    :authentication => :plain,
    :enable_starttls_auto => true
   }
