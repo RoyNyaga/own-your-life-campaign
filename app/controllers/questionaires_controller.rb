37195b8effe0f4dc1e9b557f102ed8bc47f5519b
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-04 18:23:27 +0100

    redirect to home page after the submission of questionaire

--- app/controllers/questionaires_controller.rb
+++ app/controllers/questionaires_controller.rb
@@ -2,4 +2,23 @@ class QuestionairesController < ApplicationController
   def new
   	@questionaire = Questionaire.new
   end
+
+  def create
+  	@questionaire = Questionaire.create(questionaire_params)
+	if @questionaire.save
+		flash[:success] = "Thanks for filling the questionaire #{@questionaire.name}, We will get back to you in no time."
+		redirect_to root_path
+	else
+		render "new"
+	end
+  end
+
+  private
+
+  def questionaire_params
+  	params.require(:questionaire).permit(:question_one, :question_two, 
+  		:question_three, :question_four, :question_five, :name, :phone,
+  		 :whats_app, :linkedin, :twitter, :referral_code)
+  end
+
 end
--- app/models/questionaire.rb
+++ app/models/questionaire.rb
@@ -1,2 +1,9 @@
 class Questionaire < ApplicationRecord
+	validates :question_one, presence: true
+	validates :question_two, presence: true
+	validates :question_three, presence: true
+	validates :question_four, presence: true
+	validates :question_five, presence: true
+	validates :name, presence: true
+	validates :phone, presence: true
 end
