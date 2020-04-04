cf5fa225dab58432b33b03765b6da1e36971bb52
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-04 20:29:40 +0100

    add admin user for production on seed.rb file

---
+++ app/admin/questionaires.rb
@@ -0,0 +1,7 @@
+ActiveAdmin.register Questionaire do
+
+
+  permit_params :question_one, :question_two, :question_three, :question_four, :question_seven, :name, :phone, :whats_app, :referral_code, :facebook, :question_five, :question_six
+
+  
+end
