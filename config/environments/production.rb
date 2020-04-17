cc5452eb4528e1f469672f69a91b88669832417b
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-17 23:31:56 +0100

    add environment variables for sendgrid smtp credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,14 +103,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
-   # :user_name => ENV['SENDGRID_ACCOUNT_USERNAME'],
-   # :password => ENV['SENDGRID_ACCOUNT_PASSWORD'],
-   :user_name => "itsteach",
-   :password => "just for jesus1994",
+   :user_name => ENV['SENDGRID_ACCOUNT_USERNAME'],
+   :password => ENV['SENDGRID_ACCOUNT_PASSWORD'],
    :domain => 'own-your-life-campaign.herokuapp.com',
    :address => 'smtp.sendgrid.net',
    :port => 587,
-    # :port => 465,
    :authentication => :plain,
    :enable_starttls_auto => true
   }
